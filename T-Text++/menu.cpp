44ea5272d7bc5175e8fb4821a69a751716b34632
Om Rawaley <133281331+omrawaley@users.noreply.github.com>
2023-05-19 19:31:18 -0400

    Updated License From Apache 2.0 to GPL 3.0

--- T-Text++/menu.cpp
+++ T-Text++/menu.cpp
@@ -1,18 +1,18 @@
 /*
-	Copyright 2023 Om Rawaley (@omrawaley)
+Copyright 2023 Om Rawaley (@omrawaley)
 
-    This file is part of T-Text++.
+This file is part of T-Text++.
 
-    T-Text++ is free software: you can redistribute it and/or modify it under the terms of the GNU General 
-	Public License as published by the Free Software Foundation, either version 3 of the License, or (at your 
-	option) any later version.
+T-Text++ is free software: you can redistribute it and/or modify it under the terms of the GNU General 
+Public License as published by the Free Software Foundation, either version 3 of the License, or (at your 
+option) any later version.
 
-    T-Text++ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
-	the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-	GNU General Public License for more details.
+T-Text++ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with T-Text++. If not, see 
-	<https://www.gnu.org/licenses/>. 
+You should have received a copy of the GNU General Public License along with T-Text++. If not, see 
+<https://www.gnu.org/licenses/>. 
 */
 
 #include "menu.h"
